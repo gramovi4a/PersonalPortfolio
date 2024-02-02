6cb6d432a64e072b0c818c4a978284da63044b1e
Julia <331957@viauc.dk>
2024-02-02 16:11:32 +0200

    initial NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,64 @@
+import userEvent from '@testing-library/user-event';
+import { useState, useEffect} from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import mylogo from '../assets/img/logo.png';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.png';
 
-function BasicExample() {
+
+
+export const NavBar = () => {
+    const {activeLink, setActiveLink} = useState('home');
+    const {scrolled, setScrolled} = useState(false);
+
+    useEffect (() => { // when scrolling, depending on the state, the colour of the background will be changed
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            }
+            setScrolled(false);
+        }
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [])
+
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);   
+    }
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="#home">
+            <img src={mylogo} alt="logo"></img>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"> 
+        <span className='navbar-toggler-icon'></span>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#home" className={activeLink == "home" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("home")} >Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink == "skills" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("skills")}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink == "projects" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("projects")}>Projects</Nav.Link>
           </Nav>
+          <span className='navbar-text'>
+            <div className='social-icon'>
+                <a href='https://www.linkedin.com/in/julija-gramovica/' target="_blank"><img src={navIcon1} alt='LinkedIn'/></a>
+                <a href='#'><img src={navIcon2} alt=''/></a>
+                <a href='https://github.com/gramovi4a' target="_blank"><img src={navIcon3} alt='GitHub'/></a>
+            </div>
+            <button className='vvd' onClick={() => console.log('connect')}><span>Let's connect</span></button>
+          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
 
-export default BasicExample;
+// export default BasicExample;
