415260942179e15a2df8204426a93e8c4c11c521
Julia <331957@viauc.dk>
2024-02-03 14:02:13 +0200

    initial Skills sliderr

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,64 +1,99 @@
-import userEvent from '@testing-library/user-event';
-import { useState, useEffect} from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import mylogo from '../assets/img/logo.png';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.png';
-
-
+import userEvent from "@testing-library/user-event";
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import mylogo from "../assets/img/logo.png";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.png";
 
 export const NavBar = () => {
-    const {activeLink, setActiveLink} = useState('home');
-    const {scrolled, setScrolled} = useState(false);
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
 
-    useEffect (() => { // when scrolling, depending on the state, the colour of the background will be changed
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-                setScrolled(true);
-            }
-            setScrolled(false);
-        }
+  useEffect(() => {
+    // when scrolling, depending on the state, the colour of the background will be changed
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-        window.addEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll);
 
-        return () => window.removeEventListener("scroll", onScroll);
-    }, [])
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);   
-    }
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
 
   return (
-    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
-      <Container>
-        <Navbar.Brand href="#home">
-            <img src={mylogo} alt="logo"></img>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"> 
-        <span className='navbar-toggler-icon'></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home" className={activeLink == "home" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("home")} >Home</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink == "skills" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("skills")}>Skills</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink == "projects" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("projects")}>Projects</Nav.Link>
-          </Nav>
-          <span className='navbar-text'>
-            <div className='social-icon'>
-                <a href='https://www.linkedin.com/in/julija-gramovica/' target="_blank"><img src={navIcon1} alt='LinkedIn'/></a>
-                <a href='#'><img src={navIcon2} alt=''/></a>
-                <a href='https://github.com/gramovi4a' target="_blank"><img src={navIcon3} alt='GitHub'/></a>
-            </div>
-            <button className='vvd' onClick={() => console.log('connect')}><span>Let's connect</span></button>
-          </span>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
 
-// export default BasicExample;
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={mylogo} alt="logo"></img>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink == "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink == "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink == "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Projects
+              </Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a
+                  href="https://www.linkedin.com/in/julija-gramovica/"
+                  target="_blank"
+                >
+                  <img src={navIcon1} alt="LinkedIn" />
+                </a>
+                <a href="#">
+                  <img src={navIcon2} alt="" />
+                </a>
+                <a href="https://github.com/gramovi4a" target="_blank">
+                  <img src={navIcon3} alt="GitHub" />
+                </a>
+              </div>
+              <button className="vvd" onClick={() => console.log("connect")}>
+                <span>Let's connect</span>
+              </button>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+  );
+};
---
+++ src/components/Skills.js
@@ -0,0 +1,69 @@
+// basic set up for react carousel
+import { Container, Row, Col } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png"
+
+export const Skills = () => {
+  // taken from https://www.npmjs.com/package/react-multi-carousel
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <section className="skill" id="skills">
+      <Container>
+      <Row>
+      <Col>
+            <div className="skill-bx">
+              <h2>Skills</h2>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen
+              </p>
+              <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                <div className="item">
+                  <img src={meter1} alt="Image"></img>
+                  <h5>C#</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="Image"></img>
+                  <h5>Java</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="Image"></img>
+                  <h5>JavaScript</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="Image"></img>
+                  <h5>React</h5>
+                </div>
+              </Carousel>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-left" src={colorSharp}></img>
+    </section>
+  );
+};
